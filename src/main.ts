a417bdd8d8f3b72d46ec2ea8aa0e36f0a234de9b
dodadoa <maddodadoa@gmail.com>
2021-08-15 02:25:09 +0700

    chore: make git checkout action to fetch all, format main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import "carbon-components-svelte/css/white.css";
-import App from './App.svelte';
+import App from "./App.svelte";
 
 const app = new App({
-	target: document.body,
+  target: document.body,
 });
 
 export default app;
