2c41568f96660c429aa6f669def66a8ec95097ef
dodadoa <maddodadoa@gmail.com>
2021-08-17 11:44:44 +0700

    chore: add sentry to project
    note: dsn is not secret, so we should not treat it as secret

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import "carbon-components-svelte/css/white.css";
 import App from "./App.svelte";
+import Sentry from "./util/sentry"
+
+Sentry.init()
 
 const app = new App({
   target: document.body,
